9c0b9db16bdbb24f0b3912212bcc18a7b3c4f9bb
Jeff60734 <adamraman213@gmail.com>
2026-06-19 10:24:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const fs = require("fs");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Counting bot is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
 const {
   Client,
   GatewayIntentBits,
